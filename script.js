c33655903d8af96e6d338bdc442494a3f2f2a0d0
Ziyodabonu <159676375+ziyoda-art@users.noreply.github.com>
2025-06-16 22:20:57 +0300

    .

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-
-// dropdown menu 
-const toggleBtn = document.querySelector('.navbar-toggler-icon');
-const toggleBtnIcon = document.querySelector('.navbar-toggler-icon');
-const dropDownMenu = document.querySelector('.Dropdown-menu');
-
-let isOpen = false;
-
-toggleBtn.addEventListener('click', () => {
-    dropDownMenu.style.display = isOpen ? 'none' : 'block';
-    toggleBtnIcon.className = isOpen
-        ? 'fa-solid fa-bars'
-        : 'fa-solid fa-xmark';
-    isOpen = !isOpen;
-});
-
-
 // aos 
 AOS.init();
 
