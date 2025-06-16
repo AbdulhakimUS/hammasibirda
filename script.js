c74dd568c3b980a46d5c920b646fb5add38a179a
AbdulhakimUS <127390486+AbdulhakimUS@users.noreply.github.com>
2025-06-16 19:08:25 +0500

    .

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// dropdown menu 
 const toggleBtn = document.querySelector('.navbar-toggler-icon');
 const toggleBtnIcon = document.querySelector('.navbar-toggler-icon');
 const dropDownMenu = document.querySelector('.Dropdown-menu');
@@ -12,9 +14,12 @@ toggleBtn.addEventListener('click', () => {
     isOpen = !isOpen;
 });
 
+
+// aos 
 AOS.init();
 
 
+// navbar change color 
 document.addEventListener('DOMContentLoaded', function () {
     const navbar = document.querySelector('.navbar');
     window.addEventListener('scroll', function () {
@@ -25,3 +30,17 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+// preloader
+const preloader = document.getElementById('preloader');
+  const start = Date.now();
+
+  window.addEventListener('load', function () {
+    const elapsed = Date.now() - start;
+    const delay = Math.max(2220 - elapsed, 0); // минимум 2 сек
+    setTimeout(() => {
+      preloader.classList.add('hidden');
+      setTimeout(() => preloader.remove(), 500); // удалить после анимации
+    }, delay);
+  });
