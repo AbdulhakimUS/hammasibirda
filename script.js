b3d6a3b9c6f89753a3b37427d4e1c3411daa3338
AbdulhakimUS <127390486+AbdulhakimUS@users.noreply.github.com>
2025-06-16 18:38:24 +0500

    .

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ toggleBtn.addEventListener('click', () => {
 });
 
 AOS.init();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
